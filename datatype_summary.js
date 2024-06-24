de447001ed8ff160598ee57937106d35d3e9d72e
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-06-24 17:25:39 +0000

    about memory location

---
+++ datatype_summary.js
@@ -0,0 +1,11 @@
+// about memory declaration
+
+// stack (premitive) heap(non-premitive)
+
+let first = "sameer";
+let second = first;
+
+second = "gour";
+
+console.log(first);
+console.log(second)
