30cdc7d7a4510db9eaf8e40d1bed38c196621f7d
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-06-24 17:02:54 +0000

    datatype conversion

--- fourth_conversion.js
+++ fourth_conversion.js
@@ -6,3 +6,13 @@ console.log(typeof number_score);
 // note => we can conversion like mention above 
 // use another datatype in the field of string but it should be with  first capital letter
 console.log(number_score);
+
+// about datatypes in details
+
+// primitive(value type)
+
+// 7 types: number boolean string null undefined symbol
+
+// reference type (non premitive)
+
+// object array function
