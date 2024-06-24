67dce792e5faca3522efc324e4da006e94d7b033
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-06-24 17:30:57 +0000

    about memory

--- datatype_summary.js
+++ datatype_summary.js
@@ -1,7 +1,7 @@
 // about memory declaration
 
 // stack (premitive) heap(non-premitive)
-
+// stack
 let first = "sameer";
 let second = first;
 
@@ -9,3 +9,16 @@ second = "gour";
 
 console.log(first);
 console.log(second)
+
+// heap
+
+let firstob ={
+    name: "sameer",
+    age: 23,
+    gmail: "sameer@gamil.com"
+
+}
+
+let secondOb = firstob;
+
+secondOb.gmail = "gour@gmail.com" ;
