8085e02c8e7825676c881f7cc6f06beff6537859
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-06-24 11:33:04 +0000

    configuration done

--- fourth_conversion.js
+++ fourth_conversion.js
@@ -3,3 +3,5 @@ let score = 33;
 let number_score = String(score);
 
 console.log(typeof number_score);
+// note => we can conversion like mention above 
+// use another datatype in the field of string but it should be with  first capital letter
