e09cf8c186d9245cfd6bae7e6d1b5901ad5cb508
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-06-30 10:32:31 +0000

    about string datatype

--- Stringtopic.js
+++ Stringtopic.js
@@ -2,4 +2,12 @@ let  name1 = "sameer";
 
 let value = 4;
 
-console.log(name1+value);
+console.log(name1 + value+"My lucky number");
+
+
+let name2 = "55";
+let name3 = "44";
+
+let sum = name2+name3;
+
+console.log(sum);
