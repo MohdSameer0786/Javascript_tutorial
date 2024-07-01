0e2849d52adc90f3aad6e244e1393afdf920f820
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-07-01 18:53:20 +0000

    about number datatype

--- numbertopic.js
+++ numbertopic.js
@@ -1,3 +1,23 @@
 const score = 300;
 
-console.log(score);
+//console.log(score);
+
+//console.log(typeof(score));
+
+const newScore = new Number(500);
+
+//console.log(newScore);
+
+//console.log(newScore.toString());
+
+//console.log(typeof(newScore));
+
+//console.log(newScore.toFixed(2));
+
+const otherScore = 123.99;
+
+//console.log(otherScore.toPrecision(3));
+// output is : 124
+//console.log(otherScore.toPrecision(4));
+// output is : 124.0
+console.log(otherScore.toPrecision(1));
