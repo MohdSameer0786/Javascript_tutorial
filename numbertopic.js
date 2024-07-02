d2fe593126e317eefacc3e812a33622b4ab8c53c
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-07-02 07:16:02 +0000

    about number datathype and Math library

--- numbertopic.js
+++ numbertopic.js
@@ -24,9 +24,18 @@ const newScore = new Number(500);
 
 const anotherOne = 1000000;
 
-console.log(anotherOne.toLocaleString('en-In'));
+// console.log(anotherOne.toLocaleString('en-In'));
 
 // Output will bew 10,00,000
 
 
 // -------------------Math Topic-----------------------------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// Output 4 | It gives absolute value of the given
+// console.log(Math.round(4.4));
+// Output 4 if the value is less then 4.5  like 4.4 || Output 5 if the value is greater then 4.5 like 4.6
+
+console.log(Math.random());
+// the output will come between o and 1
