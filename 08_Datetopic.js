4c81920f0338d069226cac03d4c48e8ddd92d038
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-07-02 08:07:35 +0000

    About Date library

---
+++ 08_Datetopic.js
@@ -0,0 +1,11 @@
+let mydate = new Date();
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleTimeString());
+
+let MycreatedDate = new Date(2024, 0, 7, 14, 33);
+
+console.log(MycreatedDate.toLocaleString());
