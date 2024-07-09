4980d1d1ce994281a71f4e31774075bc9f886a15
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-07-09 19:01:09 +0000

    About Date Topic

--- 08_Datetopic.js
+++ 08_Datetopic.js
@@ -1,4 +1,4 @@
-let mydate = new Date();
+//let mydate = new Date();
 
 // console.log(mydate.toString());
 // console.log(mydate.toDateString());
@@ -6,6 +6,11 @@ let mydate = new Date();
 // console.log(mydate.toLocaleDateString());
 // console.log(mydate.toLocaleTimeString());
 
-let MycreatedDate = new Date(2024, 0, 7, 14, 33);
+//let MycreatedDate = new Date(2024, 0, 7, 14, 33);
 
-console.log(MycreatedDate.toLocaleString());
+//console.log(MycreatedDate.toLocaleString());
+
+
+let newDate = new Date();
+console.log(newDate);
+//console.log(newDate.getMonth());
