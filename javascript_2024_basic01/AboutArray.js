5ae521c0a36ee259fb281760dfedc3263be55916
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-07-16 08:32:54 +0000

    About Array

---
+++ javascript_2024_basic01/AboutArray.js
@@ -0,0 +1,45 @@
+const myArray = new Array(5,3,1,4,6,2);
+
+//console.log(myArray);
+
+//const myArray2 = new Array(6,7,8);
+
+
+// Method of Array
+
+// Add in the Last of the Array using Push
+// myArray.push(9);
+
+// console.log(myArray);
+// Remove element from the Array in the Last using Pop
+// myArray.pop();
+
+// console.log(myArray);
+
+//  concatination using concat function 
+// newArray = myArray.concat(myArray2);
+
+// console.log(newArray);
+
+//
+
+// console.log(myArray.length);
+
+
+// slicing the element from the Array
+// console.log(myArray.slice(0,2));
+         // OR
+//console.log(myArray.splice(0,4));
+
+// we can sorting using sort function
+// console.log(myArray.sort())
+
+
+// And many more others method available of the Array
+
+// Remove first element of the Array And Return it
+console.log(myArray.shift(3));
+
+
+// insert a new element at the start of the array, and Return the new Length of the Array
+// console.log(myArray.unshift(0));
