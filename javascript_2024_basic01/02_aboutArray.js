3e95ba2f149b73aa52c557348112ffcb22f1b949
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-07-22 17:03:35 +0000

    about array part2

--- javascript_2024_basic01/02_aboutArray.js
+++ javascript_2024_basic01/02_aboutArray.js
@@ -1,7 +1,18 @@
 const marvel_heroes = ["Thor", "IronMan", "Hulk" ]
-const Dc_heroes = ["SuperMan", "BatMan", "Wanda"]
+const Dc_heroes = [",SuperMan", "BatMan", "Wanda"]
+const indian_heroes = [",shaktiman", "Yukudu", "Balveer"]
+//const All_heroes = marvel_heroes+Dc_heroes;
 
-All_heroes = marvel_heroes.concat(Dc_heroes)
+//console.log(All_heroes);
 
-console.log(All_heroes);
+//All_heroes = marvel_heroes.concat(Dc_heroes)
+
+ //console.log(All_heroes);
+
+//console.log(All_heroes[5]);
+
+// we can use this method to add one array to the another array 
+const new_heroes = [...marvel_heroes, ...Dc_heroes, ...indian_heroes];
+
+console.log(new_heroes);
 
