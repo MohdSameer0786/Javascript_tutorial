aad3c5abe800766467f1014ad490e337cec0984a
Mohd  Sameer <143918483+MohdSameer0786@users.noreply.github.com>
2024-08-31 06:24:40 +0000

    function variables about

---
+++ javascript_2024_basic01/05_functionVariables.js
@@ -0,0 +1,26 @@
+// function sayMyName(){
+//     console.log("sameer");
+//     console.log("gour");
+// }
+
+// sayMyName();
+
+
+// Second program 
+
+
+// function addTwonumber(a, b){
+
+//     return a+b;
+// }
+
+// console.log(addTwonumber(10,5));
+
+
+// Third Program
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("sameer"))
